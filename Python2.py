a95fe8ef4881826cbb6f7ca3de85ed7607e8ea30
Ammar sahyoun <ammarzgr@hotmail.com>
2020-04-14 09:57:17 +0200

    new python files for test

---
+++ Python2.py
@@ -0,0 +1,13 @@
+# join
+myList = ["My", "name", "is", "Ammar"]
+print("-".join(myList))
+print(type("-".join(myList)))
+
+#placeholder %
+name = "Ammar"
+age = 44
+rank = 10
+print("my name is:" + name)
+print("my name is: %s and my age is: %d and my rank is: %d" % (name, age, rank))
+
+

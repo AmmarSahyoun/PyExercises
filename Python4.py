2d473ef1ff11beeba0eca12e4681273f5ea7eaae
Ammar sahyoun <ammarzgr@hotmail.com>
2020-04-19 16:31:05 +0200

    more Dic, tuples and sets functions

--- Python4.py
+++ Python4.py
@@ -33,7 +33,7 @@ B = [1, 2, [3, 'a'], [4, 'b']]
 print(B[3][1])
 print()
 
-d = [1]     #declare list
+d = [1]  # declare list
 d.append([2, 3, 4, 5])  # add one element
 print(d)
 d.extend([6, 7, 8])  # add many elements
@@ -41,23 +41,37 @@ print(d)
 
 cloned = d[:]  # creatte cloned list from a list d
 cloned[1] = "cloned"
-print("this is original list: ",  d)
-print("this is the cloned list: " , cloned)
+print("this is original list: ", d)
+print("this is the cloned list: ", cloned)
 
 string = "   i am learning AI these days"
 print(string.split())
 print()
 
 # dictionary
-Dict={"A":1,"B":"2","C":[3,3,3],"D":(4,4,4),'E':5,'F':6}
+Dict = {"A": 1, "B": "2", "C": [3, 3, 3], "D": (4, 4, 4), 'E': 5, 'F': 6}
 print(Dict["D"])
-Dict["M"]="Ammaro"
-del(Dict["E"])
+Dict["M"] = "Ammaro"
+del (Dict["E"])
 print(Dict)
 print(Dict.keys())
 print(Dict.values())
-"Ammaro"in Dict
 
-# set
-s_set = set(d)
-print(s_set)
+print("\n#SET")
+# sets are unordered
+y = {1, 2, 3}
+i = {5, 6, "Ammor"}
+u = {"ammaro", "elderes"}
+print(y | u | i)
+o = y.copy()
+y.add(5)   # adding element doesn't affect the copied set
+print("this is set y:" , y)
+print("this is a copy of set y:" , o)
+print(y.pop())
+y.update(o)
+print("doesn't irretate same elemnts", y)
+
+print("difference between 2 sets are:", y.difference(o))
+
+var = 2 in y
+print(var)
